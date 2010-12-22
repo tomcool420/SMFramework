2571ed345e9147e4eb9d142072b830d2438c6b27
Thomas Cool <thomas.cool@me.com>
2010-12-22 20:53:40 +0100

    adding SMFSettingPreferences.h

---
+++ SMFSettingPreferences.h
@@ -0,0 +1,19 @@
+//
+//  SMFSettingPreferences.h
+//  SMFramework
+//
+//  Created by Thomas Cool on 12/19/10.
+//  Copyright 2010 tomcool.org. All rights reserved.
+//
+
+#import <Backrow/Backrow.h>
+
+
+@protocol SMFSettingPreferences
++(NSString *)displayName;
+-(NSString *)displayName;
++(BRMenuController *)settingsController;
+@optional
+-(NSString *)summary;
+-(BRImage *)art;
+@end
