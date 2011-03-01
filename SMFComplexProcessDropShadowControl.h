ffdc5a1156fbf2f47aaf07741f1fb12e036ede7c
Thomas Cool <thomas.cool@me.com>
2011-02-28 22:57:29 -0600

    adding SMFComplexProcess...

---
+++ SMFComplexProcessDropShadowControl.h
@@ -0,0 +1,16 @@
+//
+//  SMFComplexProcessDropShadowControl.h
+//  SMFramework
+//
+//  Created by Thomas Cool on 2/28/11.
+//  Copyright 2011 Thomas Cool. All rights reserved.
+//
+
+#import "SMFComplexDropShadowControl.h"
+
+
+@interface SMFComplexProcessDropShadowControl : SMFComplexDropShadowControl {
+    NSString *ap;
+}
+@property (readwrite,retain)NSString *ap;
+@end
---
+++ SMFComplexProcessDropShadowControl.m
@@ -0,0 +1,36 @@
+//
+//  SMFComplexProcessDropShadowControl.m
+//  SMFramework
+//
+//  Created by Thomas Cool on 2/28/11.
+//  Copyright 2011 Thomas Cool. All rights reserved.
+//
+
+#import "SMFComplexProcessDropShadowControl.h"
+
+
+@implementation SMFComplexProcessDropShadowControl
+@synthesize ap;
+-(void)controlWasActivated
+{
+    [super controlWasActivated];
+    [self performSelectorInBackground:@selector(runProcess) withObject:nil];
+}
+-(void)runProcess
+{
+    char line[200];
+    
+    FILE* fp = popen([ap UTF8String], "r");
+    NSMutableArray *lines = [[NSMutableArray alloc]init];
+    if (fp)
+    {
+        while (fgets(line, sizeof line, fp))
+        {
+            NSString *s = [NSString stringWithCString:line encoding:NSUTF8StringEncoding];
+            NSLog(@"s: %@",s);
+            [self performSelectorOnMainThread:@selector(appendToText:) withObject:[s stringByAppendingString:@"\n"] waitUntilDone:YES];
+        }
+    }
+    pclose(fp);
+}
+@end
